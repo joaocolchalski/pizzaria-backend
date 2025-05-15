53267c7ce53614e5966bc4bae873bc2a33119335
João Cleber Colchalski <joaocleber.jc10@gmail.com>
2025-05-15 17:48:02 -0300

    Projeto finalizado

--- src/routes.ts
+++ src/routes.ts
@@ -28,7 +28,7 @@ const router = Router();
 
 //const upload = multer(uploadConfig.upload('./tmp'));
 
-//---ROTAS USER---//
+//---ROTAS USER---///
 router.post('/users', new CreateUserController().handle);
 
 router.post('/session', new AuthUserController().handle);
